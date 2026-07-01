cbd7846a0b43da876cc50aaee46a56dce6a27db4
darklord-end <sasha35fedorov@gmail.com>
2026-07-01 16:53:18 +0700

    Changed to our servers

--- Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
+++ Telegram/SourceFiles/mtproto/mtproto_dc_options.cpp
@@ -28,8 +28,8 @@ struct BuiltInDc {
 	int port;
 };
 
-#define IpV4 "192.168.1.100"
-#define IpV6 "192.168.1.100"
+#define IpV4 "87.120.186.184"
+#define IpV6 "87.120.186.184"
 #define IpV4Port 20443
 #define IpV4Port2 20443
 #define IpV6Port 20443
